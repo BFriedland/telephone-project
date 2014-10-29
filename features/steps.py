34a8b05d784d2bd069f98064337786f6538904a0
jbbrokaw <jbrokaw@gmail.com>
2014-10-29 14:50:29 -0700

    Fiddles with some tests

--- features/steps.py
+++ features/steps.py
@@ -72,10 +72,11 @@ def can_post_prompt(step):
     assert response.status_code == 200, response.status_code
 
 
-@lettuce.step('I submit a new prompt')
-def posts_prompt(step):
+@lettuce.step('I submit a new prompt "([^"]*)"')
+def posts_prompt(step, prompt):
+    lettuce.world.expected_data = prompt
     lettuce.world.response = \
-        lettuce.world.client.post('/step_two', data={'prompt': 'New thing'})
+        lettuce.world.client.post('/step_two', data={'prompt': prompt})
 
 
 @lettuce.step('I see the second page')
@@ -87,47 +88,11 @@ def see_step_two(step):
     assert 'canvas id="drawing"' in body, msg2 % body
 
 
-# @lettuce.step('the title "([^"]*)"')
-# def title_input(step, title):
-#     lettuce.world.title = title
-
-
-# @lettuce.step('the text "([^"]*)"')
-# def text_input(step, text):
-#     lettuce.world.text = text
-
-
-# @lettuce.step('I submit the add form')
-# def add_entry(step):
-#     entry_data = {
-#         'title': lettuce.world.title,
-#         'text': lettuce.world.text,
-#     }
-#     lettuce.world.response = lettuce.world.client.post(
-#         '/add', data=entry_data, follow_redirects=False
-#     )
-
-
-# @lettuce.step('I am redirected to the home page')
-# def redirected_home(step):
-#     with app.test_request_context('/'):
-#         home_url = url_for('show_entries')
-#     # assert that we have been redirected to the home page
-#     assert lettuce.world.response.status_code in [301, 302]
-#     assert lettuce.world.response.location == 'http://localhost' + home_url
-#     # now, fetch the homepage so we can finish this off.
-#     lettuce.world.response = lettuce.world.client.get(home_url)
-
-
-# @lettuce.step('I do not see my new entry')
-# def no_new_entry(step):
-#     body = lettuce.world.response.data
-#     for val in [lettuce.world.title, lettuce.world.text]:
-#         assert val not in body
-
-
-# @lettuce.step('I see my new entry')
-# def yes_new_entry(step):
-#     body = lettuce.world.response.data
-#     for val in [lettuce.world.title, lettuce.world.text]:
-#         assert val in body
+@lettuce.step('It is put in the prompts database')
+def check_for_database(step):
+    with closing(psycopg2.connect(app.config['DATABASE'])) as db:
+        cur = db.cursor()
+        cur.execute("SELECT data FROM prompts WHERE data=%s",
+                    [lettuce.world.expected_data])
+        response = cur.fetchall()
+        print response
